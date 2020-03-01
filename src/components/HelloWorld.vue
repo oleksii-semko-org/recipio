b412d9934f9584df4bb9dded7e0b10c7045d8686
oleksii.semko <oleksii.semko@gmail.com>
2020-03-02 01:00:42 +0200

    Initial fetch implemented

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,57 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="columns medium-3" v-for="result in results" :key="result">
+      <div class="card">
+        <div class="card-divider">
+          {{ result.recipe.label }}
+        </div>
+        <div class="card-section">
+          <img :src=result.recipe.image />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+const ApiBaseUrl = "https://api.edamam.com";
+const url = "/search";
+const AppKey = "b17a326c937b31d61f448827dee723e3";
+const AppId = "b56b266a";
+
+function buildUrl (url) {
+  return `${ApiBaseUrl}${url}?q=chicken&app_id=${AppId}&app_key=${AppKey}`
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      results: []
+    }
+  },
+  mounted () {
+    this.getPosts(url);
+  },
+  methods: {
+    getPosts(section) {
+      let url = buildUrl(section);
+      axios.get(url).then((response) => {
+        console.log(this);
+        console.log(response);
+        this.results = response.data.hits;
+      }).catch( error => {error});
+    }
   }
 }
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
